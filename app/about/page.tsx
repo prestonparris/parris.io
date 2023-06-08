c552ee1a799bcf8f2bce2c49d11036d8106b310d
Preston Parris <prestonparris@gmail.com>
2023-06-08 00:44:37 -0500

    add gitops blog post

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ export default function AboutPage() {
     <section>
       <h1 className="font-bold text-3xl font-serif">About Me</h1>
       <p className="my-5 text-neutral-800 dark:text-neutral-200">
-         /// TODO
+        Trying to be a blogger, kind of? One day I will write something here. 
       </p>
     </section>
   );
